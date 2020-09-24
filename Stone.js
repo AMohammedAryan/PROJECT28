2bc33dab61dbe008209416ab7ad1156cd2c26360
AMohammedAryan <69350797+AMohammedAryan@users.noreply.github.com>
2020-09-24 16:12:13 +0530

    Update Stone.js

--- Stone.js
+++ Stone.js
@@ -3,7 +3,6 @@ class Stone extends BaseClass{
     constructor(x, y){
         super(x, y, 50, 50);
         this.image = loadImage("stone.png");    
-        Body.setStatic(this.body, true);
     }
 
     display(){
