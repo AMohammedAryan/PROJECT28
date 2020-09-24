66a893f7e50c697e4bfb40c95f33ce75b4fad11e
AMohammedAryan <69350797+AMohammedAryan@users.noreply.github.com>
2020-09-24 16:08:08 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,123 +1,114 @@
+c
 const Engine = Matter.Engine;
-const World= Matter.World;
+const World = Matter.World;
 const Bodies = Matter.Bodies;
-const Constraint = Matter.Constraint;
+const Body = Matter.Body;
 
-var engine, world;
-var box1, pig1,pig3;
-var backgroundImg,platform;
-var bird, slingshot;
+var tree, treeImage;
+var ground;
+var stone, stoneImage;
+var boyImage;
+var mango1, mango2, mango3, mango4, mangoImage;
+var constraint;
 
-var gameState = "onSling";
-
-var score = 0;
-
-function preload() {
-
-    getBackgroundImage();
+function preload()
+{
+	treeImage = loadImage("tree.png");
+	stoneImage = loadImage("stone.png");
+	boyImage = loadImage("boy.png");
+	mangoImage = loadImage("mango.png");
 }
 
-function setup(){
-    var canvas = createCanvas(1200,400);
-    engine = Engine.create();
-    world = engine.world;
+function setup() {
+	createCanvas(800, 700);
 
 
-    ground = new Ground(600,height,1200,20);
-    platform = new Ground(150, 305, 300, 170);
+	engine = Engine.create();
+	world = engine.world;
 
-    box1 = new Box(700,320,70,70);
-    box2 = new Box(920,320,70,70);
-    pig1 = new Pig(810, 350);
-    log1 = new Log(810,260,300, PI/2);
+	//Create the Bodies Here.
 
-    box3 = new Box(700,240,70,70);
-    box4 = new Box(920,240,70,70);
-    pig3 = new Pig(810, 220);
+	ground = new Ground();
 
-    log3 =  new Log(810,180,300, PI/2);
+	stone = new Stone(105, 560);
 
-    box5 = new Box(810,160,70,70);
-    log4 = new Log(760,120,150, PI/7);
-    log5 = new Log(870,120,150, -PI/7);
+	mango1 = new Mango(220, 130, 50);
+	mango2 = new Mango(200, 1, 50);
+	mango3 = new Mango(180, 30, 50);
+	mango4 = new Mango(200, 50, 50);
 
-    bird = new Bird(200,50);
+	constraint = new Constraint(stone.body, {x:105, y:560});
 
-    //log6 = new Log(230,180,80, PI/2);
-    slingshot = new SlingShot(bird.body,{x:200, y:50});
+	Engine.run(engine);
+  
 }
 
-function draw(){
 
-    if(backgroundImg){
+function draw() {
+  rectMode(CENTER);
+  background(0);
 
-        background(backgroundImg);
-    }
+  ground.display();
 
-    fill("white");
-    textSize(35);
-    text("Score:" + score, width - 300, 50);
+  image(boyImage, 180, 625, 250, 250);
 
-    Engine.update(engine);
-    //strokeWeight(4);
-    box1.display();
-    box2.display();
-    ground.display();
-    pig1.display();
-    pig1.score();
-    log1.display();
+  image(treeImage, 600, 350, 600, 700);
 
-    box3.display();
-    box4.display();
-    pig3.display();
-    pig3.score();
-    log3.display();
+  stone.display();
 
-    box5.display();
-    log4.display();
-    log5.display();
+  mango1.display();
+  mango2.display();
+  mango3.display();
+  mango4.display();
 
-    bird.display();
-    platform.display();
-    //log6.display();
-    slingshot.display();    
+  if (detectCollision(stone.body, mango1.body)){
+
+	Body.setStatic(mango1.body, false);
+  }
+
+  if (detectCollision(stone.body, mango2.body)){
+
+	Body.setStatic(mango2.body, false);
+  }
+
+  if (detectCollision(stone.body, mango3.body)){
+
+	Body.setStatic(mango3.body, false);
+  }
+
+  if (detectCollision(stone.body, mango4.body)){
+
+	Body.setStatic(mango4.body, false);
+  }
+  
+  drawSprites();
 }
 
 function mouseDragged(){
-    if (gameState!=="launched"){
-        Matter.Body.setPosition(bird.body, {x: mouseX , y: mouseY});
-    }
+
+	Body.setPosition(stone.body, {x:mouseX, y:mouseY});
 }
 
-
 function mouseReleased(){
-    slingshot.fly();
-    gameState = "launched";
+
+	constraint.fly();
 }
 
 function keyPressed(){
-    if(keyCode === 32){
-       slingshot.attach(bird.body);
-    }
+
+	if(keyCode === 32){
+
+		constraint.attach(stone.body);
+	}
 }
 
-async function getBackgroundImage(){
+function detectCollision(body1, body2){
 
-    var bg;
+    if(body1.position.x - body2.position.x <= body1.width/2 + body2.width/2 || 
+       body2.position.x - body1.position.x <= body1.width/2 + body2.width/2 ||
+       body1.position.y  - body2.position.y <= body1.height/2 + body2.height/2 ||
+       body2.position.y - body1.position.y <= body1.height/2 + body2.height/2){
 
-    var response = await fetch("http://worldtimeapi.org/api/timezone/Asia/Tokyo");
-    var responseJSON = await response.json();
-    var datetime = responseJSON.datetime;
-    var hour = datetime.slice(11, 13);
-    
-    if(hour > 06 && hour < 19){
-        
-        bg  = "sprites/bg.png";
-    }
-    else{
-
-        bg = "sprites/bg2.jpg";
-    }
-
-    backgroundImg = loadImage(bg);
+        return true;
+       }
 }
